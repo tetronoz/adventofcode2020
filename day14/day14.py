cbf4daca88dc5d2b7d6cea6d1248dda66e2b42db
Sergey Tolmachev <tetron@gmail.com>
2020-12-14 13:52:27 +0100

    fugly solutions for day14

---
+++ day14/day14.py
@@ -0,0 +1,88 @@
+import re
+import sys
+
+from collections import defaultdict
+filename = "./input/input.txt"
+
+pattern = re.compile(r"^mem\[(\d+)\]\s=\s(\d+)$")
+
+def get_value_address(line):
+    match = re.search(pattern, line)
+    value = 0
+    address = 0
+    if match:
+        address = int(match.group(1))
+        value = int(match.group(2))
+    else:
+        print("FAILED")
+        sys.exit(-1)
+    
+    return value, address
+
+def apply_mask(value, mask):
+    value_as_bin = list(bin(value)[2:])
+    zeros = 36 - len(value_as_bin)
+    full_binary_value = ['0'] * zeros
+    for v in value_as_bin:
+        full_binary_value.append(v)
+    
+    assert len(full_binary_value) == 36
+    assert len(mask) == 36
+
+    for i in range(len(mask)):
+        if mask[i] == 'X':
+            continue
+        full_binary_value[i] = mask[i]
+    
+    return int("".join(full_binary_value), 2)
+
+def get_addresses(address, mask):
+    zeros = 36 - len(address)
+    full_address = ['0'] * zeros
+    for v in address:
+        full_address.append(v)
+    
+    X = []
+
+    assert len(full_address) == 36
+    assert len(mask) == 36
+
+    for i in range(len(mask)):
+        if mask[i] == '0':
+            continue
+        if mask[i] == 'X':
+            X.append(i)
+        full_address[i] = mask[i]
+    
+    if len(X) == 0:
+        yield "".join(full_address)
+    else:
+        for v in range(2**len(X)):
+            b = list(bin(v)[2:])
+            candidate = ['0'] * (len(X) - len(b)) + b
+            for i in range(len(X)):
+                x_pos = X[i]
+                full_address[x_pos] = candidate[i]
+            yield "".join(full_address)
+
+
+def sum_values_in_memory(filename):
+    mask = None
+    mem = {}
+    mem2 = {}
+
+    with open(filename) as fp:
+        for line in fp:
+            line = line.strip()
+            if line.startswith("mask"):
+                mask = list(line.split("=")[1].lstrip())
+            else:
+                value, address = get_value_address(line)
+                new_value = apply_mask(value, mask)
+                mem[address] = new_value
+                for addr in get_addresses(list(bin(address)[2:]), mask):
+                    mem2[addr] = value
+
+    return sum(mem.values()), sum(mem2.values())
+
+print(sum_values_in_memory(filename))
